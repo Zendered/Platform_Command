f62de0a5d8f6fc2f7c1eee28acc5fbb8f777fa9b
zend <luizz.ggr@gmail.com>
2023-05-18 15:59:21 -0300

    Add project files.

---
+++ Src/Services/PlatformService.API/Models/Platform.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlatformService.API.Models
+{
+    public class Platform
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Publisher { get; set; }
+        public string Cost { get; set; }
+    }
+}
