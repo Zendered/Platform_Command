6a19dd28528b327733b5cd8a8155550468877cb7
zend <luizz.ggr@gmail.com>
2023-05-18 16:38:59 -0300

    adding in-memory database

---
+++ Src/Services/PlatformService.API/ProgramExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PlatformService.API
+{
+    public static class ProgramExtensions
+    {
+        public static void AddCustomDataBase(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<DbContext>(opt =>
+                opt.UseInMemoryDatabase("InMem"));
+        }
+        public static void AddCustomRequestPipeLine(this WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+        }
+    }
+}
