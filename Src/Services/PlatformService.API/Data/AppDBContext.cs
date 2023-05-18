8f569d8cea0fbf29ebfefe1ffa1b465e48f1e572
zend <luizz.ggr@gmail.com>
2023-05-18 17:01:59 -0300

    appdbcontext

---
+++ Src/Services/PlatformService.API/Data/AppDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.API.Models;
+
+namespace PlatformService.API.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public DbSet<Platform> Platforms { get; set; }
+
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+
+        }
+    }
+}
