68f6d33caf1c334282bf2cfc8764b153793a3d8f
zend <luizz.ggr@gmail.com>
2023-05-18 17:01:30 -0300

    implementing repository

---
+++ Src/Services/PlatformService.API/Data/Repository/IPlatformRepo.cs
@@ -0,0 +1,13 @@
+﻿using PlatformService.API.Models;
+
+namespace PlatformService.API.Data.Repo
+{
+    public interface IPlatformRepo
+    {
+        bool SaveChanges();
+
+        IEnumerable<Platform> GetAllPlatforms();
+        Platform GetPlatformById(int id);
+        void CreatePlatform(Platform platform);
+    }
+}
