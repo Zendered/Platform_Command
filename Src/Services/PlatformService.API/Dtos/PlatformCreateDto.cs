b6f593c3c20538b5f98be47bcea962a9a9b0c512
zend <luizz.ggr@gmail.com>
2023-06-14 17:36:33 -0300

    create/read dto

--- Src/Services/PlatformService.API/Dtos/PlatformCreateDto.cs
+++ Src/Services/PlatformService.API/Dtos/PlatformCreateDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace PlatformService.API.Dtos
 {
-    public class PlatformCreatDto
+    public class PlatformCreateDto
     {
         public required string Name { get; set; }
         public required string Publisher { get; set; }
