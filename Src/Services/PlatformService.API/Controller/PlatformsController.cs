5012b858e7f81d15817396bbfba0eb299d2fbf96
zend <luizz.ggr@gmail.com>
2023-06-14 18:36:10 -0300

    Platforms Controller

---
+++ Src/Services/PlatformService.API/Controller/PlatformsController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.API.Data.Repository;
+using PlatformService.API.Dtos;
+
+namespace PlatformService.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepo _platformRepo;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(IPlatformRepo platformRepo, IMapper mapper)
+        {
+            _platformRepo = platformRepo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetAllPlatforms()
+        {
+            Console.WriteLine("--> Getting Platforms");
+            var platformItems = _platformRepo.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
